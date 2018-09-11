6802158fad6096ea90ba75ca45dc7c4f5b6a37f5
ChihYuChiang <ckccyu@gmail.com>
2018-09-11 10:59:17 +0800

    Add: react routing with para

--- assignment/react-app/src/components/DishdetailComponent.js
+++ assignment/react-app/src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         return(
@@ -44,11 +45,16 @@ import { Card, CardImg, CardText, CardBody,
             return(
                 <div class="container">
                     <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                    </div>
+                    <div className="row">
                         <div className="col-12 col-md-5 m-1">
                             <RenderDish dish={props.dish} />
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderComments  comments={props.dish.comments} />
+                            <RenderComments  comments={props.comments} />
                         </div>
                     </div>
                 </div>
