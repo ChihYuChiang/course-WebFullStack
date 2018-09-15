22727df66fd3119d722f3d7a81ffcd5590215bee
ChihYuChiang <ckccyu@gmail.com>
2018-09-15 19:12:37 +0800

    Add: fetch

--- assignment/react-app/src/components/DishdetailComponent.js
+++ assignment/react-app/src/components/DishdetailComponent.js
@@ -8,6 +8,7 @@ import {
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -16,7 +17,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
 function RenderDish({ dish }) {
     return(
         <Card>
-            <CardImg top src={dish.image} alt={dish.name} />
+            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
