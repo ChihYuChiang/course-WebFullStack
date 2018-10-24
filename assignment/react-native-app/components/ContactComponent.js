ab92cfdd7b620e0fcdd97a785b4e152029322b0b
ChihYuChiang <ckccyu@gmail.com>
2018-10-24 08:35:58 +0800

    Add: react native animation

--- assignment/react-native-app/components/ContactComponent.js
+++ assignment/react-native-app/components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 
 class Contact extends Component {
@@ -11,10 +12,11 @@ class Contact extends Component {
 
     render() {
         return (
-            <Card
-                title='Contact Information'>
-                <Text
-                    style={{ margin: 10 }}>                    
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>                
+                <Card
+                    title='Contact Information'>
+                    <Text
+                        style={{ margin: 10 }}>                    
 {`121, Clear Water Bay Road
 
 Clear Water Bay, Kowloon
@@ -26,8 +28,9 @@ Tel: +852 1234 5678
 Fax: +852 8765 4321
 
 Email:confusion@food.net`}                    
-                </Text>
-            </Card>
+                    </Text>
+                </Card>
+            </Animatable.View>
         );
     }
 }
